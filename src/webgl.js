33213bafd913556f9a2395a5c60e95eccab3f250
7gutes7 <7gutes7@gmail.com>
2026-07-19 02:49:32 -0600

    fix: set targetCamZ = 7.0 (+2 units from base 5.0) in Memoria Natural section

--- src/webgl.js
+++ src/webgl.js
@@ -1080,8 +1080,8 @@ export class SimbiotikWebGL {
         this.logoSpinSpeed = 0.005;
         break;
       case 'memoria-intro':
-        // Logo 3D centrado en el fondo y alejado 30 unidades sobre Z (targetCamZ = 35.0)
-        targetCamZ = 35.0;
+        // Logo 3D centrado en el fondo y alejado 2 unidades sobre Z respecto a la base (targetCamZ = 7.0)
+        targetCamZ = 7.0;
         targetCamX = 0.0;
         targetCamY = 0.0;
         this.logoSpinSpeed = 0.002;
